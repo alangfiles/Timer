df9f511a3acb63f32ad974fe79adcf85bb180a09
Alan Files <afiles@igsenergy.com>
2018-03-22 15:57:14 -0400

    adding electron stuff

---
+++ main.js
@@ -0,0 +1,86 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow, tray;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow(
+    {
+      width: 800,
+      height: 600,
+      minHeight: 400,
+      minWidth: 600,
+      backgroundColor: '#CCCCCC',
+      show: false,
+      icon: path.join(__dirname, 'assets/icons/png/Timer2.png_128x128.png'),
+      skipTaskbar: true
+    })
+  //mainWindow.setFullScreen(true);
+  mainWindow.setMenu(null);
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+
+  mainWindow.on('ready-to-show', () => mainWindow.show());
+}
+
+
+function createTray() {
+  tray = new electron.Tray('assets/icons/png/Timer2.png_64x64.png')
+  const contextMenu = electron.Menu.buildFromTemplate([
+    { label: '1 minute', click() { } },
+    { label: '5 minutes', click() { } },
+    { label: '20 minutes', click() { } },
+    { type: 'separator' },
+    { label: 'Close Application', click() { closeApplication() } }
+  ])
+  tray.setToolTip('This is my application.');
+  tray.setContextMenu(contextMenu);
+  tray.on('click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  })
+}
+
+function closeApplication() {
+  mainWindow = null;
+  app.quit();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+
+app.on('ready', () => {
+  createWindow();
+  createTray();
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  tray.displayBalloon({ title: 'Moved', content: 'moved to notification bar' })
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
